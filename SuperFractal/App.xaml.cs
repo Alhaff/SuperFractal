21ff974bfde57c5f578a57a1a2acc54c95cd8b72
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-05-12 15:56:48 +0300

    Added main window logic

--- SuperFractal/App.xaml.cs
+++ SuperFractal/App.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using SuperFractal.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +15,13 @@ namespace SuperFractal
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel()
+            };
+            MainWindow.Show();  
+        }
     }
 }
