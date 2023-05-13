e6e6d730cc74831da1ef1700a22a9e7ba482c2ea
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-05-13 18:20:38 +0300

    Added fractal stores

---
+++ SuperFractal/Stores/FractalStore.cs
@@ -0,0 +1,31 @@
+﻿using AlgebraicFractals;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperFractal.Stores
+{
+    public class FractalStore
+    {
+        public List<AlgebraicFractal> Fractals { get; set; }
+        public CreateMultiFractal MultiFractalCreator { get; set; }
+        public Dictionary<string,CreateMultiFractal> MultiFractalsCreators { get; init; }
+        public FractalStore() 
+        { 
+            Fractals = new List<AlgebraicFractal>();
+            MultiFractalsCreators = new Dictionary<string, CreateMultiFractal>()
+            {
+                {"Simple", AlgebraicFractal.CreateMultiFractalSimple},
+                {"Simple in TreadPool", AlgebraicFractal.CreateMultiFractalSimpleInThreadPool },
+                {"AVX2", AlgebraicFractal.CreateMultiFractalIntrinsics },
+                {"AVX2 in ThreadPool", AlgebraicFractal.CreateMultiFractalIntrinsicsInThreadPool }
+            };
+            MultiFractalCreator = AlgebraicFractal.CreateMultiFractalIntrinsicsInThreadPool;
+        }
+        public int ThreadCount { get; set; } = 64;
+        public int MaxIterations { get; set; } = 64;
+    }
+}
