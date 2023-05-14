65c98798735a4c0c0f738509e0e70ee447d679b5
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-05-14 12:05:33 +0300

    Added edit scene view model and view

---
+++ SuperFractal/ViewModels/EditSceneViewModel.cs
@@ -0,0 +1,27 @@
+﻿using AlgebraicFractals;
+using SuperFractal.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperFractal.ViewModels
+{
+    public class EditSceneViewModel : ViewModelBase
+    {
+        private readonly NavigationStore _navigationStore;
+        public ViewModelBase CurrentViewModel  => _navigationStore.CurrentViewModel;
+
+        public EditSceneViewModel(NavigationStore navigationStore)
+        {
+            _navigationStore = navigationStore;
+
+            _navigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
+        }
+        private void OnCurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrentViewModel));
+        }
+    }
+}
