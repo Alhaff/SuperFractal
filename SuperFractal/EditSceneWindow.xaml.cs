2e29c8e39c2cdaa8440595011d78a44aa85bb776
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-05-15 01:35:08 +0300

    Style changes

--- SuperFractal/EditSceneWindow.xaml.cs
+++ SuperFractal/EditSceneWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace SuperFractal
     /// <summary>
     /// Interaction logic for EditSceneWindow.xaml
     /// </summary>
-    public partial class EditSceneWindow : Window
+    public partial class EditSceneWindow : MetroWindow
     {
         public EditSceneWindow()
         {
