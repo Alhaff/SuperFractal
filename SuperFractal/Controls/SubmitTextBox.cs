d989795dce7f57376984a36d5250ac78ac632b6d
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-05-15 01:33:47 +0300

    Added custom controls

---
+++ SuperFractal/Controls/SubmitTextBox.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows;
+
+namespace SuperFractal.Controls
+{
+    public class SubmitTextBox : TextBox
+    {
+        public SubmitTextBox()
+            : base()
+        {
+            PreviewKeyDown += new KeyEventHandler(SubmitTextBox_PreviewKeyDown);
+            FontStyle = System.Windows.FontStyles.Italic;
+            TextAlignment = System.Windows.TextAlignment.Center;
+        }
+
+        void SubmitTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                BindingExpression be = GetBindingExpression(TextBox.TextProperty);
+                if (be != null)
+                {
+                    be.UpdateSource();
+                }
+            }
+        }
+    }
+}
